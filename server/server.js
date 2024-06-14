dc2e7535d18ebc77cd8fe4138878ff9f6ce98a3b
Hemanth Sahukari <sahukarihemanth586@gmail.com>
2024-06-14 18:14:35 +0530

    changed the value of origin to "*"

--- server/server.js
+++ server/server.js
@@ -13,7 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin : 'http://localhost:5173',
+    // origin : 'http://localhost:5173',
+    origin:"*",
     credentials:true
 }))
 app.get('/',(req,res) => {
